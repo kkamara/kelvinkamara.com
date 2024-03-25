77a3297ff901f5768d7fba5dcdba3be3039d4ae3
kkamara <kamaracomputers@gmail.com>
2024-03-25 23:56:49 +0000

    Use double quote strings

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-Route::middleware(['throttle:api'])->group(function () {
+Route::middleware(["throttle:api"])->group(function () {
     Route::post("contact", [ContactController::class, "contact"])
         ->name("contact");
 });
