a76826b05ed96e1b14c241f3af5c251d7a9f6137
kkamara <kamaracomputers@gmail.com>
2025-02-26 20:14:58 +0000

    Remove API throttling

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 
-Route::middleware(["throttle:api"])->group(function () {
-    Route::post("contact", [ContactController::class, "contact"])
-        ->name("contact");
-});
+Route::post("contact", [ContactController::class, "contact"])
+    ->name("contact");
